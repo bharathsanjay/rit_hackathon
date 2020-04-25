d96244292e8116c79800dbdff041a01a5347a2c9
Akshay Chandru <31569582+akspow@users.noreply.github.com>
2020-04-25 14:08:44 +0530

    Add files via upload
    Image Extraction module still in testing

---
+++ Python Scripts/news.py
@@ -0,0 +1,78 @@
+from newsapi.newsapi_client import NewsApiClient
+from textblob import TextBlob
+from datetime import datetime,timedelta
+
+
+def display(newsfeed):
+    for i in newsfeed:
+        print("\n".join(i))
+    print("\n") 
+
+def sentiment(news,value):
+    #newsfile = open('newsfile.txt','w')
+    newsfeed = []
+    for i in news['articles']:
+        article = TextBlob(i['description'])
+        if(article.sentiment.polarity > value):
+            #newsfeed.append(i)
+            newsfeed.append([i['source']['name'],i['title'],i['description']])
+            #[i['publishedAt']
+            """print(i['source']['name'])
+            print(i['title'])
+            print(i['description'])
+            rint("---------------------------------------------------")
+            newsfile.write(i['source']['name'])
+            newsfile.write("\n")
+            newsfile.write(i['title'])
+            newsfile.write("\n")
+            newsfile.write(i['description'])
+            newsfile.write("\n")
+            newsfile.write("---------------------------------------------------\n")"""
+            
+    #returns newssource, title, and description as 2D array          
+    return newsfeed
+
+    #newsfile.close()
+
+def newssources():
+    s = []
+    #fs = open('sources.txt','w')
+    sources = news.get_sources()
+    for source in sources['sources']:
+        #fs.write(source['name'])
+        #fs.write('\n')
+        s.append(source['name'])
+    #fs.close()
+    return s
+
+#15 day news feed
+
+today = datetime.utcnow().date()
+duration = datetime.utcnow().date() - timedelta(days = 15)
+
+#API KEY
+
+news = NewsApiClient(api_key = "0de6787ea7e44a8691ce4a5d556d18dc")
+
+#News Sources
+
+#sources = newssources()
+
+#News extraction
+
+top_headlines = news.get_everything(q='covid OR corona OR success OR recovered OR lockdown OR quarantine OR recovery',
+                                      from_param=duration,
+                                      language='en')
+"""url = ('http://newsapi.org/v2/everything?'
+       'q=corona&covid'
+       'from=2020-04-24&'
+       'sortBy=popularity&'
+       'apiKey=0de6787ea7e44a8691ce4a5d556d18dc')
+top_headlines = requests.get(url)"""
+
+#sentiment analysis using Textblob
+
+newsfeed = sentiment(top_headlines,0.2)
+
+#display(newsfeed)
+#print(newsfeed)
