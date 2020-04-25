a591654d4856a99201984dbd2036346b56a1729b
Akshay Chandru <31569582+akspow@users.noreply.github.com>
2020-04-25 19:19:15 +0530

    Update news.py

--- Python Scripts/news.py
+++ Python Scripts/news.py
@@ -7,7 +7,7 @@ def sentiment(news,value):
     newsfeed = []
     for i in news['articles']:
         article = TextBlob(i['description'])
-        if(article.sentiment.polarity > value):
+        if(article.sentiment.polarity >= value):
             t = imagextraction.imgextract(i['title'][:20])
             newsfeed.append([i['source']['name'],i['title'],i['description'],str(t)])
     #returns newssource, title, description and image          
@@ -24,7 +24,7 @@ news = NewsApiClient(api_key = "0de6787ea7e44a8691ce4a5d556d18dc")
 
 #News extraction
 
-top_headlines = news.get_everything(q='covid OR corona OR success OR recovered OR lockdown OR quarantine OR recovery',
+top_headlines = news.get_everything(q='covid OR corona OR success OR recovered OR lockdown OR quarantine OR recovery NOT positive NOT Deadlier NOT Dead',
                                       from_param=duration,
                                       language='en')
 
